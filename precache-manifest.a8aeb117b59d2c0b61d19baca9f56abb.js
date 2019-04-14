dc0c88395c1d63df80ccac07ae96082603500380
Jakub Urban <iakub.urban@gmail.com>
2019-04-15 00:39:56 +0100

    Updates

---
+++ precache-manifest.a8aeb117b59d2c0b61d19baca9f56abb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fd47887dd2be0d3e877",
+    "url": "/static/css/main.cb506e0b.chunk.css"
+  },
+  {
+    "revision": "9fd47887dd2be0d3e877",
+    "url": "/static/js/main.9466bfc8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5854cd3954dccab224f3",
+    "url": "/static/js/2.2c9e84b5.chunk.js"
+  },
+  {
+    "revision": "de868d2a469da37985b6eb310b280934",
+    "url": "/index.html"
+  }
+];
