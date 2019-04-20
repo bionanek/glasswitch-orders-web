e1c8898fdb72f7e62ff2d10029dbf69f28cc377e
Tomas Oplatek <oplatek.t@gmail.com>
2019-04-20 16:47:51 +0100

    Added remaining customer properties to edit form, CustomerEdit is now responsive

--- src/components/customers/CustomerEdit.js
+++ src/components/customers/CustomerEdit.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import dummyCustomers from "./CustomersData";
+import './CustomerEdit.scss';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 
 class CustomerEdit extends Component {
   constructor(props) {
@@ -19,35 +23,114 @@ class CustomerEdit extends Component {
   }
 
   handleSubmit(event) {
-    alert("form submitted");
+    //TODO: Call API and update the item
+    console.log("Form submitted");
   }
 
   render() {
+    const customer = this.state.customer;
+    //TODO: Add validations
     return (
-      <Form
-        onSubmit={e => this.handleSubmit(e)}
-      >
-        <Form.Group controlId="formName">
-          <Form.Label>Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter your name" defaultValue={this.state.customer.name}/>
-        </Form.Group>
-        <Form.Group controlId="formEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" defaultValue={this.state.customer.email}/>
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-        <Form.Group controlId="formPhone">
-          <Form.Label>Phone number</Form.Label>
-          <Form.Control type="text" placeholder="Enter your phone number" defaultValue={this.state.customer.phone}/>
-        </Form.Group>
-        <Form.Group controlId="formVat">
-          <Form.Label>VAT identification number</Form.Label>
-          <Form.Control type="text" placeholder="Enter your VAT" defaultValue={this.state.customer.vatNumber}/>
-        </Form.Group>
-        <Button type="submit">Submit form</Button>
-      </Form>
+      <div className="edit-new-form">
+        {customer ?
+          (
+            <Container>
+              <Form
+                onSubmit={e => this.handleSubmit(e)}
+              >
+                <Row>
+                  <Col sm>
+                    <Form.Group controlId="formName">
+                      <Form.Label>Name</Form.Label>
+                      <Form.Control type="text" placeholder="Enter your name" defaultValue={customer.name} />
+                    </Form.Group>
+                  </Col>
+                  <Col sm>
+                    <Form.Group controlId="formEmail">
+                      <Form.Label>Email address</Form.Label>
+                      <Form.Control type="email" placeholder="Enter email" defaultValue={customer.email} />
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col sm>
+                    <Form.Group controlId="formPhone">
+                      <Form.Label>Phone number</Form.Label>
+                      <Form.Control type="text" placeholder="Enter your phone number" defaultValue={customer.phone} />
+                    </Form.Group>
+                  </Col>
+                  <Col sm>
+                    <Form.Group controlId="formVat">
+                      <Form.Label>VAT identification number</Form.Label>
+                      <Form.Control type="text" placeholder="Enter your VAT" defaultValue={customer.vatNumber} />
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <h3>Delivery Address</h3>
+                <Row>
+                  <Col sm>
+                    <Form.Group controlId="formDeliveryStreet">
+                      <Form.Label>Street</Form.Label>
+                      <Form.Control type="text" placeholder="Enter the street" defaultValue={customer.delivery_street} />
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col sm>
+                    <Form.Group controlId="formDeliveryCity">
+                      <Form.Label>City</Form.Label>
+                      <Form.Control type="text" placeholder="Enter the city" defaultValue={customer.delivery_city} />
+                    </Form.Group>
+                  </Col>
+                  <Col sm>
+                    <Form.Group controlId="formDeliveryCountry">
+                      <Form.Label>Country</Form.Label>
+                      <Form.Control type="text" placeholder="Enter the country" defaultValue={customer.delivery_country} />
+                    </Form.Group>
+                  </Col>
+                  <Col sm>
+                    <Form.Group controlId="formDeliveryPostcode">
+                      <Form.Label>Postcode</Form.Label>
+                      <Form.Control type="text" placeholder="Enter the postcode" defaultValue={customer.delivery_postCode} />
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <h3>Billing Address</h3>
+                <Row>
+                  <Col sm>
+                    <Form.Group controlId="formBillingStreet">
+                      <Form.Label>Street</Form.Label>
+                      <Form.Control type="text" placeholder="Enter the street" defaultValue={customer.billing_street} />
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col sm>
+                    <Form.Group controlId="formBillingCity">
+                      <Form.Label>City</Form.Label>
+                      <Form.Control type="text" placeholder="Enter the city" defaultValue={customer.billing_city} />
+                    </Form.Group>
+                  </Col>
+                  <Col sm>
+                    <Form.Group controlId="formBillingCountry">
+                      <Form.Label>Country</Form.Label>
+                      <Form.Control type="text" placeholder="Enter the country" defaultValue={customer.billing_country} />
+                    </Form.Group>
+                  </Col>
+                  <Col sm>
+                    <Form.Group controlId="formBillingPostcode">
+                      <Form.Label>Postcode</Form.Label>
+                      <Form.Control type="text" placeholder="Enter the postcode" defaultValue={customer.billing_postCode} />
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Button type="submit">Submit form</Button>
+              </Form>
+            </Container>
+          ) :
+          (<span>Customer with ID: {this.state.customerId} does not exist!</span>)
+        }
+      </div>
     )
   }
 }
--- src/components/customers/Customers.js
+++ src/components/customers/Customers.js
@@ -6,10 +6,6 @@ import "./Customers.scss";
 const Customers = () => {
   return (
     <div className="customers">
-      <header className="customers-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>List Of Customers</h1>
-      </header>
       <CustomersList className="customers-list" />
     </div>
   );
