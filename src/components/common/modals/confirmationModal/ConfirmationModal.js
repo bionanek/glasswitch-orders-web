09a9dea84e4d59852631b969d4214f7636302b42
Jakub Urban <iakub.urban@gmail.com>
2019-05-07 01:30:16 +0100

    create initial version of ConfirmationModal and use it in simplelistelement

---
+++ src/components/common/modals/confirmationModal/ConfirmationModal.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
+
+export default function ConfirmationModal(props) {
+	const title = props.title ? props.title : 'Are you sure?'
+	const message = props.message
+		? props.message
+		: 'Are you sure you want to continue? After deleting the item is gone!'
+
+	const handleModalClose = () => {
+		props.onModalClose()
+	}
+
+	return (
+		<div>
+			<Modal show={props.isOpen} onHide={() => handleModalClose()}>
+				<Modal.Header closeButton>
+					<Modal.Title>{title}</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>{message}</Modal.Body>
+				<Modal.Footer>
+					<Button variant="secondary" onClick={() => handleModalClose()}>
+						Close
+					</Button>
+					<Button variant="primary" onClick={() => handleModalClose()}>
+						Save Changes
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		</div>
+	)
+}
--- src/components/common/simpleList/SimpleListElement.js
+++ src/components/common/simpleList/SimpleListElement.js
@@ -1,42 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SimpleListElement.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrashAlt, faEdit } from '@fortawesome/free-solid-svg-icons'
+import ConfirmationModal from '../modals/confirmationModal/ConfirmationModal'
 
 const SimpleListElement = props => {
+	const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false)
 	const isDeletable = props.isDeletable === undefined ? true : props.isDeletable
 
 	const onDeleteButtonClick = e => {
 		e.stopPropagation()
-		if (props.element.deleteHandler) {
-			props.element.deleteHandler(props.element.id)
-		} else {
-			props.defaultOnDeleteClick(props.element, props.index)
-		}
+		setIsDeleteModalOpen(!isDeleteModalOpen)
+		// return
+		// if (props.element.deleteHandler) {
+		// 	props.element.deleteHandler(props.element.id)
+		// } else {
+		// 	props.defaultOnDeleteClick(props.element, props.index)
+		// }
+	}
+
+	const closeDeleteModal = () => {
+		setIsDeleteModalOpen(false)
 	}
 
 	return (
-		<li
-			className={`list-element ${
-				props.isClickable && props.element.clickHandler ? 'clickable' : ''
-			}`}
-			onClick={props.element.clickHandler}
-		>
-			<span className="title">{props.element.name.toString()}</span>
-			<span className="sub-title">{props.element.id.toString()}</span>
-			<span className="buttons-wrapper">
-				{props.isEditable && (
-					<span className="edit-icon" onClick={props.element.editHandler} role="button">
-						<FontAwesomeIcon icon={faEdit} />
-					</span>
-				)}
-				{isDeletable && (
-					<span className="delete-icon" onClick={onDeleteButtonClick} role="button">
-						<FontAwesomeIcon icon={faTrashAlt} />
-					</span>
-				)}
-			</span>
-		</li>
+		<>
+			<li
+				className={`list-element ${
+					props.isClickable && props.element.clickHandler ? 'clickable' : ''
+				}`}
+				onClick={props.element.clickHandler}
+			>
+				<span className="title">{props.element.name.toString()}</span>
+				<span className="sub-title">{props.element.id.toString()}</span>
+				<span className="buttons-wrapper">
+					{props.isEditable && (
+						<span className="edit-icon" onClick={props.element.editHandler} role="button">
+							<FontAwesomeIcon icon={faEdit} />
+						</span>
+					)}
+					{isDeletable && (
+						<span className="delete-icon" onClick={onDeleteButtonClick} role="button">
+							<FontAwesomeIcon icon={faTrashAlt} />
+						</span>
+					)}
+				</span>
+			</li>
+			<ConfirmationModal isOpen={isDeleteModalOpen} onModalClose={closeDeleteModal} />
+		</>
 	)
 }
 
