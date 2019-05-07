f40d5cb13804a60c6c207abfb84f6e563155ab35
@elvixpix <oskarkaczmarzyk@pm.me>
2019-05-07 18:06:03 +0200

    refactored product-detail page into function

--- src/components/products/crud/ProductDetail.js
+++ src/components/products/crud/ProductDetail.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
@@ -6,74 +6,64 @@ import DetailElement from '../../common/DetailElement'
 import ProductsApiService from '../../../utils/api/productsApiService'
 import './ProductDetail.scss'
 
-export default class ProductDetail extends Component {
-	constructor(props) {
-		super(props)
+export default function ProductDetail(props) {
+	const [product, setProduct] = useState(null)
 
-		this.state = {
-			product: null,
+	useEffect(() => {
+		const fetchData = async () => {
+			const fetchedProduct = await ProductsApiService.getProductById(props.match.params.id)
+			setProduct(fetchedProduct.data)
 		}
-	}
 
-	async componentDidMount() {
-		this.setState({ product: await this.getProductById() })
-	}
+		fetchData()
+	}, [])
 
-	async getProductById() {
-		const response = await ProductsApiService.getProductById(this.props.match.params.id)
-		return response.data
-	}
+	return (
+		<div className="product-detail">
+			{product ? (
+				<Container>
+					<Row>
+						<Col>
+							<h1>{product.name}</h1>
+						</Col>
+					</Row>
 
-	render() {
-		const { product } = this.state
+					<Row>
+						<DetailElement header="Description:" value={product.description} />
+						<DetailElement header="Type:" value={product.type} />
+						<DetailElement header="Category:" value={product.category} />
+					</Row>
 
-		return (
-			<div className="product-detail">
-				{product ? (
-					<Container>
-						<Row>
-							<Col>
-								<h1>{product.name}</h1>
-							</Col>
-						</Row>
+					<Row>
+						<Col>
+							<h2>Parameters (cm)</h2>
+						</Col>
+					</Row>
 
-						<Row>
-							<DetailElement header="Description:" value={product.description} />
-							<DetailElement header="Type:" value={product.type} />
-							<DetailElement header="Category:" value={product.category} />
-						</Row>
+					<Row>
+						<DetailElement header="Width:" value={product.width} />
+						<DetailElement header="Height:" value={product.height} />
+						<DetailElement header="Depth:" value={product.depth} />
+					</Row>
 
-						<Row>
-							<Col>
-								<h2>Parameters (cm)</h2>
-							</Col>
-						</Row>
+					<Row>
+						<Col>
+							<h2>Prices</h2>
+						</Col>
+					</Row>
 
-						<Row>
-							<DetailElement header="Width:" value={product.width} />
-							<DetailElement header="Height:" value={product.height} />
-							<DetailElement header="Depth:" value={product.depth} />
-						</Row>
-
-						<Row>
-							<Col>
-								<h2>Prices</h2>
-							</Col>
-						</Row>
-
-						<Row>
-							<DetailElement header="Polish Zloty (PLN):" value={product.price.pln} />
-							<DetailElement header="Euro (EUR):" value={product.price.eur} />
-							<DetailElement header="US Dollar (USD):" value={product.price.usd} />
-						</Row>
-					</Container>
-				) : (
-					<span>
-						Product with ID:
-						{this.props.match.params.id} does not exists!
-					</span>
-				)}
-			</div>
-		)
-	}
+					<Row>
+						<DetailElement header="Polish Zloty (PLN):" value={product.price.pln} />
+						<DetailElement header="Euro (EUR):" value={product.price.eur} />
+						<DetailElement header="US Dollar (USD):" value={product.price.usd} />
+					</Row>
+				</Container>
+			) : (
+				<span>
+					Product with ID:
+					{props.match.params.id} does not exists!
+				</span>
+			)}
+		</div>
+	)
 }
