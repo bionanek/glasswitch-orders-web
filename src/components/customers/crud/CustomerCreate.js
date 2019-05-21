7fafbd445b0d7d71732067fcea538b138df4d7c7
Tomas Oplatek <oplatek.t@gmail.com>
2019-05-21 17:24:30 +0100

    Refactored CustomerCreate into function component

--- src/components/customers/crud/CustomerCreate.js
+++ src/components/customers/crud/CustomerCreate.js
@@ -1,30 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
 import CustomersApiService from '../../../utils/api/customersApiService'
 import CustomerForm from './CustomerForm'
 
-class CustomerCreate extends Component {
-  onCancel() {
-    this.props.history.push(`/customers`)
+const CustomerCreate = (props) => {
+  function onCancel() {
+    props.history.push(`/customers`)
   }
 
-  async handleSubmit(event, customer) {
+  async function handleSubmit(event, customer) {
     event.preventDefault()
     event.stopPropagation()
 
     const response = await CustomersApiService.createCustomer(customer)
 
-    this.props.history.push(`/customers/${response.id}`)
+    props.history.push(`/customers/${response.id}`)
   }
 
-  render() {
-    return (
-      <CustomerForm
-        customer={{}}
-        onSubmit={(e, cust) => this.handleSubmit(e, cust)}
-        onCancel={() => this.onCancel()}
-      />
-    )
-  }
+  return (
+    <CustomerForm
+      customer={{}}
+      onSubmit={handleSubmit}
+      onCancel={onCancel}
+    />
+  )
 }
 
 export default CustomerCreate
